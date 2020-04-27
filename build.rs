2323f269eec6a9dcb3af215b4a3b3829db599075
David Schatz <david.schatz@tu-ilmenau.de>
2020-04-27 17:00:44 +0200

    Add CMake to x448 C lib

--- build.rs
+++ build.rs
@@ -1,24 +1,9 @@
 fn main() {
-    // TODO make this work
-    // pkg_config::Config::new()
-    //     .atleast_version("1.0")
-    //     .probe("crypto")
-    //     .unwrap();
+    // build static x448 lib
+    let dst = cmake::build("include/x448");
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-lib=static=x448");
 
-    let x448_src = [
-        "include/x448/src/arch_32/f_impl.c",
-        "include/x448/src/curve448.c",
-        "include/x448/src/curve448_tables.c",
-        "include/x448/src/f_generic.c",
-        "include/x448/src/scalar.c",
-    ];
-    let mut x448_builder = cc::Build::new();
-    let x448_build = x448_builder
-        .files(x448_src.iter())
-        .include("include/x448/include")
-        .include("include/x448/src")
-        .flag("-Wno-unused-parameter");
-    x448_build.compile("x448");
-
+    // compile protobuf files
     tonic_build::compile_protos("protobuf/directory.proto").expect("Failed to generate gRPC");
 }
