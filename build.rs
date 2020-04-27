4187009b455ddf96990636c2ef6c5c02e4048426
David Schatz <david.schatz@tu-ilmenau.de>
2020-04-27 16:02:10 +0200

    Strip and restructure copied openssl code to get rid of link dependency

--- build.rs
+++ build.rs
@@ -6,21 +6,19 @@ fn main() {
     //     .unwrap();
 
     let x448_src = [
-        "include/x448/arch_32/f_impl.c",
-        "include/x448/curve448.c",
-        "include/x448/curve448_tables.c",
-        "include/x448/eddsa.c",
-        "include/x448/f_generic.c",
-        "include/x448/scalar.c",
+        "include/x448/src/arch_32/f_impl.c",
+        "include/x448/src/curve448.c",
+        "include/x448/src/curve448_tables.c",
+        "include/x448/src/f_generic.c",
+        "include/x448/src/scalar.c",
     ];
     let mut x448_builder = cc::Build::new();
     let x448_build = x448_builder
         .files(x448_src.iter())
-        .include("include/x448")
+        .include("include/x448/include")
+        .include("include/x448/src")
         .flag("-Wno-unused-parameter");
     x448_build.compile("x448");
-
-    println!("cargo:rustc-link-lib=crypto");
 
     tonic_build::compile_protos("protobuf/directory.proto").expect("Failed to generate gRPC");
 }
