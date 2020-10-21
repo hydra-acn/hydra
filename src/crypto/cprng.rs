3030eb0eae426412f2bbbd5301ec1cdac786554a
David Schatz <david.schatz@tu-ilmenau.de>
2020-10-21 15:51:57 +0200

    Use cprng from rand crate instead of openssl (much faster)

--- src/crypto/cprng.rs
+++ src/crypto/cprng.rs
@@ -1,29 +1,6 @@
-//! Wrapping OpenSSL random number generation for convenient use
-use rand::Rng;
-use rand_core::{impls, CryptoRng, Error, RngCore};
-
-struct Cprng {}
-
-impl CryptoRng for Cprng {}
-
-impl RngCore for Cprng {
-    fn fill_bytes(&mut self, dest: &mut [u8]) {
-        openssl::rand::rand_bytes(dest).expect("openssl could not seed CPRNG");
-    }
-
-    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
-        Ok(self.fill_bytes(dest))
-    }
-
-    fn next_u32(&mut self) -> u32 {
-        impls::next_u32_via_fill(self)
-    }
-
-    fn next_u64(&mut self) -> u64 {
-        impls::next_u64_via_fill(self)
-    }
-}
+//! Wrapping `ThreadRng` from `rand` library, which actually is a CPRNG in version 0.7.
+use rand::{thread_rng, CryptoRng, Rng};
 
 pub fn thread_cprng() -> impl Rng + CryptoRng {
-    Cprng {}
+    thread_rng()
 }
