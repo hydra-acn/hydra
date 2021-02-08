032e2323cfee991c71be9858f7ad216435b7bfd7
David Schatz <david.schatz@tu-ilmenau.de>
2021-02-08 16:27:22 +0100

    Fix mix config: don't crash when optional config file is missing

--- src/mix/cfg.rs
+++ src/mix/cfg.rs
@@ -1,19 +1,21 @@
 use config::{ConfigError, File};
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Default)]
+#[serde(default)]
 pub struct FirebaseConfig {
     pub server_auth_key: Option<String>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Default)]
+#[serde(default)]
 pub struct Config {
     pub firebase: FirebaseConfig,
 }
 
 impl Config {
     pub fn new(cfg_file: Option<&str>) -> Result<Self, ConfigError> {
-        let mut cfg = config::Config::default();
+        let mut cfg = config::Config::new();
         if let Some(f) = cfg_file {
             cfg.merge(File::with_name(f))?;
         }
